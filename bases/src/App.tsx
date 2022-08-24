79ce075e6b8f8c01ff60262383b977ccacd71f8e
juanCamilo <juanmu@bextsa.com>
2022-08-23 21:47:46 -0500

    Chore: added Counter component using useState

--- bases/src/App.tsx
+++ bases/src/App.tsx
@@ -1,9 +1,11 @@
+import {Counter} from "./bases/Counter";
 
 function App() {
     return (
         <>
-          <h1>React</h1>
+            <h1>React bases</h1>
             <hr/>
+            <Counter initialValue={170}/>
         </>
     );
 }
